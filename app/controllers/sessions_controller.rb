c45ff6d1386ce273da2d519075b9dccf9f03d3ac
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-25 12:11:41 -0500

    sessions controller allows sign in sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     user = User.find_by username: params[:username]
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
