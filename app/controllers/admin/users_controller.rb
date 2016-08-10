4654cbd5ec43ab32647f5f96827b90aef95523c1
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-10 13:10:49 -0500

    only admin can assign user to a case

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,45 +18,19 @@ class Admin::UsersController < ApplicationController
 
 
     def create
-     @user = User.new
-     @user.email = params[:user][:email]
-     @user.role = params[:user][:role]
-     @user.username = params[:user][:username]
-     @user.password = params[:user][:password]
-     @user.first_name = params[:user][:first_name]
-     @user.middle_name = params[:user][:middle_name]
-     @user.last_name = params[:user][:last_name]
-     @user.maiden_name = params[:user][:maiden_name]
-     @user.full_name = params[:user][:full_name]
-     @user.dob = params[:user][:dob]
-     @user.profile_image = params[:user][:profile_image]
-     @user.password_confirmation = params[:user][:password_confirmation]
+     @user = User.create(user_params)
        if @user.save
-         # we should also sign them in
-         session[:user_id] = @user.id
-         redirect_to root_path, notice: "Welcome!"
+         redirect_to root_path, notice: "User Added!"
        else
          render :new
        end
      end
 
 
-
   def update
     @user = User.find_by id: params[:id]
-    @user.email = params[:user][:email]
-    @user.role = params[:user][:role]
-    @user.username = params[:user][:username]
-    @user.password = params[:user][:password]
-    @user.full_name = params[:user][:full_name]
-    @user.first_name = params[:user][:first_name]
-    @user.middle_name =params[:user][:middle_name]
-    @user.last_name = params[:user][:last_name]
-    @user.maiden_name = params[:user][:maiden_name]
-    @user.dob = params[:user][:dob]
-    @user.profile_image = params[:user][:profile_image]
-      if @user.save
-        redirect_to root_path, notice: "Profile Updated!"
+    if @user.update(user_params)
+        redirect_to root_path, notice: "User Updated!"
       else
         render :edit
       end
