fc95b823ecdba4a5bebc5c67998c1007890907c5
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-10 14:21:02 -0500

    remove controller tests

--- test/controllers/admin/dashboard_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Admin::DashboardController do
-  it "should get show" do
-    get admin_dashboard_show_url
-    value(response).must_be :success?
-  end
-
-end
--- test/controllers/user_matters_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-describe UserMattersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-end
