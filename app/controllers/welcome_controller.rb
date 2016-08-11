d114286abbea7a73c266ae9b03c5074ca9bb7fcc
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-11 14:46:10 -0500

    update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,8 @@
 class WelcomeController < ApplicationController
-  before_action except: :hello do
-    if @current_user.nil?
-      redirect_to sign_in_path, alert: "Please Sign In"
-    end
-  end
 
   def hello
-    @user = User.find_by id: session[:user_id]
+  end
+
+  def about
   end
 end
