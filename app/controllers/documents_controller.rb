da74383980aab5f370551199626e0bd5fe9172ca
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-11 00:16:08 -0500

    document form rendering for attorney & clients

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,64 +1,62 @@
 class DocumentsController < ApplicationController
-  before_action do
-    if @current_user.nil?
-      redirect_to sign_in_path, alert: "Please Sign In"
+    before_action do
+        redirect_to sign_in_path, alert: 'Please Sign In' if @current_user.nil?
     end
-  end
 
-  def show
-    @document = Document.find_by id: params[:id]
-  end
-
-  def index
-    @documents = @current_user.documents
-  end
-
-  def new
-    @document = Document.new
-  end
-
-  def create
-    @document = Document.new(document_params)
-    if @document.save
-      redirect_to root_path, notice: "Document Added"
-      else
-        render :new
-      end
-  end
-
-  def edit
-    @document = Document.find_by id: params[:id]
-  end
-
-  def update
-    @document = Document.find_by id: params[:id]
-
-     if @document.update(params.require(:document).permit(:description, :document, :document_id, :matter_id, :description, :category, :year, :month, :day, :client_notes, :attorney_notes, :law_office_notes, :year, :hot_doc, :issues, :sort_date))
-      redirect_to document_path(id: @document.id), notice: "Document Updated!"
-    else
-      render :edit
+    def show
+        @document = Document.find_by id: params[:id]
     end
-  end
 
-  def upload
-    @document = Document.find_by id: params[:id]
-
-     if @document.update(params.require(:document).permit(:description, :document, :document_id, :matter_id, :description, :category, :year, :month, :day, :client_notes, :attorney_notes, :law_office_notes, :year, :hot_doc, :issues, :sort_date))
-      redirect_to document_path(id: @document.id), notice: "Document Uploaded!"
-    else
-      render :edit
+    def index
+        @documents = @current_user.documents
     end
-  end
 
-  def delete
-    @document = @current_user.documents.find_by! id: params[:id]
-    @document.destroy
-    redirect_to root_path, notice: "Document Deleted"
-  end
+    def new
+        @document = Document.new
+    end
 
-  private
+    def create
+        @document = Document.new(document_params)
+        if @document.save
+            redirect_to root_path, notice: 'Document Added'
+        else
+            render :new
+          end
+    end
 
-  def document_params
-    params.require(:document).permit(:description, :document, :document_id, :matter_id, :description, :category, :year, :month, :day, :client_notes, :attorney_notes, :law_office_notes, :year, :hot_doc, :issues, :sort_date)
-  end
+    def edit
+        @document = Document.find_by id: params[:id]
+    end
+
+    def update
+        @document = Document.find_by id: params[:id]
+
+        if @document.update(params.require(:document).permit(:description, :document, :document_id, :matter_id, :description, :category, :year, :month, :day, :client_notes, :attorney_notes, :law_office_notes, :year, :hot_doc, :issues, :sort_date))
+            redirect_to document_path(id: @document.id), notice: 'Document Updated!'
+        else
+            render :edit
+       end
+    end
+
+    def upload
+        @document = Document.find_by id: params[:id]
+
+        if @document.update(params.require(:document).permit(:description, :document, :document_id, :matter_id, :description, :category, :year, :month, :day, :client_notes, :attorney_notes, :law_office_notes, :year, :hot_doc, :issues, :sort_date))
+            redirect_to document_path(id: @document.id), notice: 'Document Uploaded!'
+        else
+            render :edit
+       end
+    end
+
+    def delete
+        @document = @current_user.documents.find_by! id: params[:id]
+        @document.destroy
+        redirect_to root_path, notice: 'Document Deleted'
+    end
+
+    private
+
+    def document_params
+        params.require(:document).permit(:description, :document, :document_id, :matter_id, :description, :category, :year, :month, :day, :client_notes, :attorney_notes, :law_office_notes, :year, :hot_doc, :issues, :sort_date)
+    end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,5 @@ class Document < ApplicationRecord
     has_many :users, through: :matters
     attachment :document, destroy: false, extension: 'pdf'
     CATEGORY = ['Child: Health', 'Child: Education', 'Child: Other', 'Financials: Assets', 'Financials: Income', 'Financials: Liabilities', 'Financials: Expenses', 'Drug Tests', 'Infidelity', 'Criminal', 'Emails', 'Phone Records', 'Text Messages', 'Social Media'].freeze
-    MONTH = %w(01 02 03 04 05 06 07 08 09 10 11 12).freeze
-    DAY = %w(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31).freeze
     STATUS = %w(None Requested Provided Reviewed Produced).freeze
 end
