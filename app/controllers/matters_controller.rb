89c0eb458bf0f675b0071765c1bb304feb916b0a
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-09-04 11:39:18 -0500

    update matters controller for strong params

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -14,20 +14,7 @@ class MattersController < ApplicationController
   end
 
   def create
-    @matters = Matter.new
-    @matters.cause_number = params[:matter][:cause_number]
-    @matters.casename = params[:matter][:casename]
-    @matters.year = params[:matter][:year]
-    @matters.client_id = params[:matter][:client_id]
-    @matters.client = params[:matter][:client]
-    @matters.attorney = params[:matter][:attorney]
-    @matters.paralegal = params[:matter][:paralegal]
-    @matters.kind = params[:matter][:kind]
-    @matters.status = params[:matter][:status]
-    @matters.opposing_party = params[:matter][:opposing_party]
-    @matters.opposing_counsel = params[:matter][:opposing_counsel]
-    @matters.judge = params[:matter][:judge]
-    @matters.court = params[:matter][:court]
+    @matters = Matter.create(matter_params)
     if @matters.save
         redirect_to root_path, notice: "Matter Added!"
     else render :new
@@ -47,11 +34,10 @@ class MattersController < ApplicationController
     @documents = @matters.documents.all
     @people = @matters.people.all
     @photos = @matters.photos.all
-
     @client = @matters.users.by_role("Client")
     @attorney = @matters.users.by_role("Attorney")
     @paralegal = @matters.users.by_role("Law Office")
-end
+  end
 
   def edit
     @matters = Matter.find_by id: params[:id]
@@ -59,23 +45,14 @@ end
 
   def update
      @matters = Matter.find_by id: params[:id]
-     @matters.cause_number = params[:matter][:cause_number]
-     @matters.casename = params[:matter][:casename]
-     @matters.year = params[:matter][:year]
-     @matters.client_id = params[:matter][:client_id]
-     @matters.client = params[:matter][:client]
-     @matters.attorney = params[:matter][:attorney]
-     @matters.paralegal = params[:matter][:paralegal]
-     @matters.kind = params[:matter][:kind]
-     @matters.opposing_party = params[:matter][:opposing_party]
-     @matters.opposing_counsel = params[:matter][:opposing_counsel]
-     @matters.judge = params[:matter][:judge]
-     @matters.court = params[:matter][:court]
-     @matters.status = params[:matter][:status]
-     if @matters.save
+     @matters.update(matter_params)
+       if @matters.save
          redirect_to dashboard_path, notice: "Matter Updated!"
      else render :update
      end
- end
-
+  end
+  private
+  def matter_params
+    params.require(:matter).permit(:id, :cause_number, :casename, :year, :client_id, :client, :attorney, :paralegal, :kind, :opposing_party, :opposing_counsel, :judge, :court, :status)
+  end
 end
